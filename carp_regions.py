2d0afb60fac5867008cd2d15c23dddd019991eeb
jonah37 <dajonahfr@gmail.com>
2025-03-15 11:14:30 -0700

    ros2 pkg

---
+++ carp_regions.py
@@ -0,0 +1,9 @@
+#Associate region number with image
+images = {
+    "0": r'C:\Users\jonah\Downloads\Region 0.jpg',
+    "1": r'C:\Users\jonah\Downloads\Region 1.jpg',
+    "2": r'C:\Users\jonah\Downloads\Region 2.jpg',
+    "3": r'C:\Users\jonah\Downloads\Region 3.jpg',
+    "4": r'C:\Users\jonah\Downloads\Region 4.jpg',
+    "5": r'C:\Users\jonah\Downloads\Region 5.jpg'
+}
